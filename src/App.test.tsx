17a962ff727a253f92f805a6c3d0804fa521fa6d
Andrés Moschini <andresmoschini@gmail.com>
2021-12-14 14:58:51 -0300

    test: add tests for routing

---
+++ src/App.test.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import App from "./App";
+import { MemoryRouter } from "react-router-dom";
+
+const rootURL = "/";
+const expensesURL = "/expenses";
+const invoicesURL = "/invoices";
+const wrongURL = "/wrong/url";
+const expensesContent = "Expenses content";
+const invoicesContent = "Invoices content";
+const notFoundContent = "There's nothing here!";
+
+test("root URL should not render invoices or expenses content", async () => {
+  // Arrange & Act
+  const initialURL = rootURL;
+  render(
+    <MemoryRouter initialEntries={[initialURL]}>
+      <App />
+    </MemoryRouter>
+  );
+
+  // Assert
+  const expensesContentElements = screen.queryByText(expensesContent);
+  expect(expensesContentElements).toBeNull();
+  const invoiceContentElements = screen.queryByText(invoicesContent);
+  expect(invoiceContentElements).toBeNull();
+  const notFoundContentElements = screen.queryByText(notFoundContent);
+  expect(notFoundContentElements).toBeNull();
+});
+
+test("Wrong URL should render expected content", async () => {
+  // Arrange & Act
+  const initialURL = wrongURL;
+  const expectedContent = notFoundContent;
+  render(
+    <MemoryRouter initialEntries={[initialURL]}>
+      <App />
+    </MemoryRouter>
+  );
+
+  // Assert
+  screen.getByText(expectedContent);
+});
+
+test("expenses URL should render expected content", async () => {
+  // Arrange & Act
+  const initialURL = expensesURL;
+  const expectedContent = "Expenses content";
+  render(
+    <MemoryRouter initialEntries={[initialURL]}>
+      <App />
+    </MemoryRouter>
+  );
+
+  // Assert
+  screen.getByText(expectedContent);
+});
+
+test("invoices URL should render expected content", async () => {
+  // Arrange & Act
+  const initialURL = invoicesURL;
+  const expectedContent = "Invoices content";
+  render(
+    <MemoryRouter initialEntries={[initialURL]}>
+      <App />
+    </MemoryRouter>
+  );
+
+  // Assert
+  screen.getByText(expectedContent);
+});
+
+test("Click in first 'Expenses' link should should show expenses title", async () => {
+  // Arrange
+  const initialURL = invoicesURL;
+  render(
+    <MemoryRouter initialEntries={[initialURL]}>
+      <App />
+    </MemoryRouter>
+  );
+  const linkElement = screen.getAllByText(/Expenses/i)[0];
+
+  // Act
+  linkElement.click();
+
+  // Assert
+  await waitFor(() => screen.getByText(expensesContent));
+});
--- src/routes/invoices.tsx
+++ src/routes/invoices.tsx
@@ -1,7 +1,7 @@
 export default function Invoices() {
   return (
     <main style={{ padding: "1rem 0" }}>
-      <h2>Invoices</h2>
+      <h2>Invoices content</h2>
     </main>
   );
 }
