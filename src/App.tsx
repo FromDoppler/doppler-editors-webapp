5c6ac0ae67c3b15a3159e2876517ed6d3fce9406
Andrés Moschini <andresmoschini@gmail.com>
2021-12-14 13:03:11 -0300

    feat: add external links for demo purposes

--- src/App.tsx
+++ src/App.tsx
@@ -28,9 +28,24 @@ function App() {
             paddingBottom: "1rem",
           }}
         >
-          {/* Links to this own app (using history API) */}
-          <Link to="/invoices">Invoices</Link> |{" "}
-          <Link to="/expenses">Expenses</Link> | <Link to="/wrong">Wrong</Link>
+          <div>
+            {/* Links to this own app (using history API) */}
+            <Link to="/invoices">Invoices</Link> |{" "}
+            <Link to="/expenses">Expenses</Link> |{" "}
+            <Link to="/wrong">Wrong</Link>
+          </div>
+          <div>
+            {/* links to this own app (using real browser navigation, assuming that `editors` is the
+            base path) */}
+            <a href="/editors/invoices">Invoices</a> |{" "}
+            <a href="/editors/expenses">Expenses</a> |{" "}
+            <a href="/editors/wrong">Wrong</a>
+          </div>
+          <div>
+            {/* links to Doppler WebApp app (assuming that domain is shared) */}
+            <a href="/login">Login</a> | <a href="/signup">Sign Up</a> |{" "}
+            <a href="/dashboard">Dashboard</a> | <a href="/wrong">Wrong</a>
+          </div>
           <Outlet />
         </nav>
       </div>
