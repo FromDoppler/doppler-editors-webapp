103306d15361838c41bd4d38cb740a45ad1c511f
Andrés Moschini <andresmoschini@gmail.com>
2021-12-16 12:38:09 -0300

    chore: add an easy way to start demo page

---
+++ demo/start-demo.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+print_help () {
+    echo ""
+    echo "Usage: sh start-demo.sh"
+    echo ""
+    echo "Starts a localhost server to use online version of Doppler Editors WebApp."
+    echo "You can edit index.html in order to use a different version, see detains in the code."
+}
+
+set -e
+set -u
+
+cd "$(dirname "$0")"
+
+npx http-server . -o /editors/ --proxy http://localhost:8080?
