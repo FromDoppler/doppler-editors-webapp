7742d56089f78c7bbcd2ea257eb64317b48bb3e8
Andrés Moschini <andresmoschini@gmail.com>
2021-12-17 21:17:57 -0300

    refactor: remove defaults React imports

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
 import { MemoryRouter } from "react-router-dom";
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link, Outlet } from "react-router-dom";
 import logo from "./logo.svg";
 import "./Main.css";
