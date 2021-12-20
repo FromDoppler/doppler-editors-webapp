81b836d4f1c2a54b9a5db02ca6fbd171960c333f
Cromeror <cristobalromerorossi@gmail.com>
2021-12-20 10:00:53 -0500

    feat: add unlayer editor

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { Main } from "./Main";
 import { Expenses } from "./routes/expenses";
 import { Invoices } from "./routes/invoices";
+import Campaigns from "./routes/campaigns";
+import Templates from "./routes/templates";
 
 export const App = () => (
   <Routes>
     <Route path="/" element={<Main />}>
+      <Route path="campaigns/:idCampaign" element={<Campaigns />} />
+      <Route path="templates/:idTemplate" element={<Templates />} />
       <Route path="expenses" element={<Expenses />} />
       <Route path="invoices" element={<Invoices />} />
       <Route
---
+++ src/routes/templates.tsx
@@ -0,0 +1,7 @@
+import { Editor } from "../components/Editor";
+
+const Templates = () => {
+  return <Editor></Editor>;
+};
+
+export default Templates;
