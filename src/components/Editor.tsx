3f4d2285c9c967183c604c4640e4783d1fa68c68
Andrés Moschini <andresmoschini@gmail.com>
2021-12-23 09:28:08 -0300

    refactor: add types to deal missing properties in unlayer types

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -2,6 +2,14 @@ import { useEffect, useRef } from "react";
 import EmailEditor, { Design, UnlayerOptions, User } from "react-email-editor";
 import { mergeTags } from "../external/merge.tags";
 
+interface ExtendedUnlayerOptions extends UnlayerOptions {
+  mergeTagsConfig: { sort: boolean };
+}
+
+interface ExtendedUnlayerUser extends User {
+  signature?: string;
+}
+
 interface EditorProps {
   design?: Design;
 }
@@ -22,21 +30,21 @@ export const Editor = (props: EditorProps) => {
 
   const userId: number = parseInt(process.env.REACT_APP_USER_ID as string, 10);
   const userSignature: string = process.env.REACT_APP_USER_SIGNATURE as string;
-  const userExtend = {
+  const user: ExtendedUnlayerUser = {
     id: userId,
     signature: userSignature,
-  } as User;
-  const UnlayerOptionsExtended = {
+  };
+  const unlayerOptions: ExtendedUnlayerOptions = {
     mergeTagsConfig: {
       sort: false,
     },
     mergeTags: mergeTags,
-    user: userExtend,
+    user: user,
     customJS: [
       `${process.env.PUBLIC_URL}/customJs/index.js`,
       `window.setCompanyTitle("Making Sense");`,
     ],
-  } as UnlayerOptions;
+  };
 
   return (
     <div style={{ height: "calc(100% - 70px)", display: "flex" }}>
@@ -45,7 +53,7 @@ export const Editor = (props: EditorProps) => {
         projectId={projectId}
         key="email-editor-test"
         ref={emailEditorRef}
-        options={UnlayerOptionsExtended}
+        options={unlayerOptions}
       />
     </div>
   );
