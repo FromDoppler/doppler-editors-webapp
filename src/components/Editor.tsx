fbfb7374337c39330e6c1f1b6cee29458e0dc833
Andrés Moschini <andresmoschini@gmail.com>
2021-12-23 10:09:22 -0300

    refactor: read unlayer projectId from configuration

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef } from "react";
 import EmailEditor, { Design, UnlayerOptions, User } from "react-email-editor";
+import { AppServices } from "../abstractions";
 import { mergeTags } from "../external/merge.tags";
+import { InjectAppServices } from "./AppServicesContext";
 
 interface ExtendedUnlayerOptions extends UnlayerOptions {
   mergeTagsConfig: { sort: boolean };
@@ -10,51 +12,56 @@ interface ExtendedUnlayerUser extends User {
   signature?: string;
 }
 
-interface EditorProps {
-  design?: Design;
-}
-
-export const Editor = (props: EditorProps) => {
-  const { design } = props;
-  const emailEditorRef = useRef<EmailEditor>();
-  const projectId: number = parseInt(
-    process.env.REACT_APP_PROJECT_ID as string,
-    10
-  );
-
-  useEffect(() => {
-    if (design && emailEditorRef.current) {
-      emailEditorRef.current.loadDesign(design);
-    }
-  });
-
-  const userId: number = parseInt(process.env.REACT_APP_USER_ID as string, 10);
-  const userSignature: string = process.env.REACT_APP_USER_SIGNATURE as string;
-  const user: ExtendedUnlayerUser = {
-    id: userId,
-    signature: userSignature,
-  };
-  const unlayerOptions: ExtendedUnlayerOptions = {
-    mergeTagsConfig: {
-      sort: false,
+export const Editor = InjectAppServices(
+  ({
+    appServices: {
+      appConfiguration: { unlayerProjectId },
     },
-    mergeTags: mergeTags,
-    user: user,
-    customJS: [
-      `${process.env.PUBLIC_URL}/customJs/index.js`,
-      `window.setCompanyTitle("Making Sense");`,
-    ],
-  };
+    design,
+  }: {
+    appServices: AppServices;
+    design?: Design;
+  }) => {
+    const emailEditorRef = useRef<EmailEditor>();
 
-  return (
-    <div style={{ height: "calc(100% - 70px)", display: "flex" }}>
-      <EmailEditor
-        style={{ minHeight: "100%" }}
-        projectId={projectId}
-        key="email-editor-test"
-        ref={emailEditorRef as React.MutableRefObject<EmailEditor>}
-        options={unlayerOptions}
-      />
-    </div>
-  );
-};
+    useEffect(() => {
+      if (design && emailEditorRef.current) {
+        emailEditorRef.current.loadDesign(design);
+      }
+    });
+
+    const userId: number = parseInt(
+      process.env.REACT_APP_USER_ID as string,
+      10
+    );
+    const userSignature: string = process.env
+      .REACT_APP_USER_SIGNATURE as string;
+    const user: ExtendedUnlayerUser = {
+      id: userId,
+      signature: userSignature,
+    };
+    const unlayerOptions: ExtendedUnlayerOptions = {
+      mergeTagsConfig: {
+        sort: false,
+      },
+      mergeTags: mergeTags,
+      user: user,
+      customJS: [
+        `${process.env.PUBLIC_URL}/customJs/index.js`,
+        `window.setCompanyTitle("Making Sense");`,
+      ],
+    };
+
+    return (
+      <div style={{ height: "calc(100% - 70px)", display: "flex" }}>
+        <EmailEditor
+          style={{ minHeight: "100%" }}
+          projectId={unlayerProjectId}
+          key="email-editor-test"
+          ref={emailEditorRef as React.MutableRefObject<EmailEditor>}
+          options={unlayerOptions}
+        />
+      </div>
+    );
+  }
+);
--- src/default-configuration.ts
+++ src/default-configuration.ts
@@ -6,4 +6,8 @@ export const defaultAppConfiguration: AppConfiguration = {
   // Original WebApp shares the same domain than Editors WebApp. So, it is not
   // necessary to specify the domain, and the path is shared across environments
   loginPageUrl: "/login",
+  unlayerProjectId:
+    (process.env.REACT_APP_PROJECT_ID &&
+      parseInt(process.env.REACT_APP_PROJECT_ID, 10)) ||
+    0,
 };
