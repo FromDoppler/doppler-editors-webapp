b0074cab00479e31bc7ae87930dca22f14ba7e7a
Andrés Moschini <andresmoschini@gmail.com>
2021-12-23 14:13:23 -0300

    refactor: set default projectId directly in defaultAppConfiguration

--- src/default-configuration.ts
+++ src/default-configuration.ts
@@ -6,8 +6,5 @@ export const defaultAppConfiguration: AppConfiguration = {
   // Original WebApp shares the same domain than Editors WebApp. So, it is not
   // necessary to specify the domain, and the path is shared across environments
   loginPageUrl: "/login",
-  unlayerProjectId:
-    (process.env.REACT_APP_PROJECT_ID &&
-      parseInt(process.env.REACT_APP_PROJECT_ID, 10)) ||
-    0,
+  unlayerProjectId: 32092,
 };
