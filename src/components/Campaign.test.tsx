8c303be6175a67736a3c5e74306b98d6c2b3cab4
Andrés Moschini <andresmoschini@gmail.com>
2022-01-04 17:30:56 -0300

    feat: load campaign content in Campaign component

---
+++ src/components/Campaign.test.tsx
@@ -0,0 +1,131 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import { AppServices } from "../abstractions";
+import { HtmlEditorApiClient } from "../abstractions/html-editor-api-client";
+import { AppServicesProvider } from "./AppServicesContext";
+import { Campaign, errorMessageTestId, loadingMessageTestId } from "./Campaign";
+
+const emailEditorPropsTestId = "EmailEditor_props";
+
+const baseAppServices = {
+  appSessionStateAccessor: {
+    current: {
+      status: "authenticated",
+      jwtToken: "jwtToken",
+      dopplerAccountName: "dopplerAccountName",
+      unlayerUser: {
+        id: "unlayerUserId",
+        signature: "unlayerUserSignature",
+      },
+    },
+  },
+  appConfiguration: {
+    unlayerProjectId: 12345,
+    unlayerEditorManifestUrl: "unlayerEditorManifestUrl",
+    loaderUrl: "loaderUrl",
+  },
+} as AppServices;
+
+describe(Campaign.name, () => {
+  it("should show loading and then error when getCampaignContent is not successful", async () => {
+    // Arrange
+    const idCampaign = "1234";
+
+    let resolveGetCampaignContentPromise: any;
+    const getCampaignContent = jest.fn(
+      () =>
+        new Promise((resolve) => {
+          resolveGetCampaignContentPromise = resolve;
+        })
+    );
+
+    const htmlEditorApiClient = {
+      getCampaignContent,
+    } as unknown as HtmlEditorApiClient;
+
+    // Act
+    render(
+      <AppServicesProvider
+        appServices={{ ...baseAppServices, htmlEditorApiClient }}
+      >
+        <MemoryRouter initialEntries={[`/${idCampaign}`]}>
+          <Routes>
+            <Route path="/:idCampaign" element={<Campaign />} />
+          </Routes>
+        </MemoryRouter>
+      </AppServicesProvider>
+    );
+
+    // Assert
+    expect(getCampaignContent).toHaveBeenCalledWith(idCampaign);
+
+    screen.getByTestId(loadingMessageTestId);
+    screen.getByTestId(emailEditorPropsTestId);
+
+    const errorMessageEl = screen.queryByTestId(errorMessageTestId);
+    expect(errorMessageEl).toBeNull();
+
+    // Act
+    resolveGetCampaignContentPromise({ success: false });
+
+    // Assert
+    await screen.findByTestId(errorMessageTestId);
+
+    const loadingMessageEl = screen.queryByTestId(loadingMessageTestId);
+    expect(loadingMessageEl).toBeNull();
+
+    const emailEditorEl = screen.queryByTestId(emailEditorPropsTestId);
+    expect(emailEditorEl).toBeNull();
+  });
+
+  it("should show EmailEditor when the getCampaignContent is successful", async () => {
+    // Arrange
+    const idCampaign = "1234";
+
+    let resolveGetCampaignContentPromise: any;
+    const getCampaignContent = jest.fn(
+      () =>
+        new Promise((resolve) => {
+          resolveGetCampaignContentPromise = resolve;
+        })
+    );
+
+    const htmlEditorApiClient = {
+      getCampaignContent,
+    } as unknown as HtmlEditorApiClient;
+
+    // Act
+    render(
+      <AppServicesProvider
+        appServices={{ ...baseAppServices, htmlEditorApiClient }}
+      >
+        <MemoryRouter initialEntries={[`/${idCampaign}`]}>
+          <Routes>
+            <Route path="/:idCampaign" element={<Campaign />} />
+          </Routes>
+        </MemoryRouter>
+      </AppServicesProvider>
+    );
+
+    // Assert
+    screen.getByTestId(loadingMessageTestId);
+    screen.getByTestId(emailEditorPropsTestId);
+
+    const errorMessageEl = screen.queryByTestId(errorMessageTestId);
+    expect(errorMessageEl).toBeNull();
+
+    expect(getCampaignContent).toHaveBeenCalledWith(idCampaign);
+
+    // Act
+    resolveGetCampaignContentPromise({ success: true, value: {} });
+
+    // Assert
+    await screen.findByTestId(emailEditorPropsTestId);
+
+    const loadingMessageEl = screen.queryByTestId(loadingMessageTestId);
+    expect(loadingMessageEl).toBeNull();
+
+    const errorMessageEl2 = screen.queryByTestId(errorMessageTestId);
+    expect(errorMessageEl2).toBeNull();
+  });
+});
--- src/components/Campaign.tsx
+++ src/components/Campaign.tsx
@@ -1,5 +1,66 @@
-import { Editor } from "./Editor";
+import { useEffect, useState } from "react";
+import { Design } from "react-email-editor";
+import { useParams } from "react-router-dom";
+import { UnexpectedError } from "../abstractions/common/result-types";
+import { Editor } from "../components/Editor";
+import { useAppServices } from "./AppServicesContext";
+
+type LoadingDesignState =
+  | { loading: true; error: null; design: null }
+  | { error: UnexpectedError; loading: false; design: null }
+  | { design: Design; loading: false; error: null };
+
+export const loadingMessageTestId = "loading-message";
+export const errorMessageTestId = "error-message";
 
 export const Campaign = () => {
-  return <Editor design={null}></Editor>;
+  const { htmlEditorApiClient } = useAppServices();
+  const { idCampaign } = useParams();
+
+  const [state, setState] = useState<LoadingDesignState>({
+    loading: true,
+    error: null,
+    design: null,
+  });
+
+  useEffect(() => {
+    const loadDesign = async () => {
+      if (!idCampaign) {
+        setState({
+          error: { success: false, unexpectedError: "Missing idCampaign" },
+          loading: false,
+          design: null,
+        });
+      } else {
+        const result = await htmlEditorApiClient.getCampaignContent(idCampaign);
+        if (result.success) {
+          setState({ design: result.value, loading: false, error: null });
+        } else {
+          setState({
+            error: result.unexpectedError,
+            loading: false,
+            design: null,
+          });
+        }
+      }
+    };
+    loadDesign();
+  }, [idCampaign, htmlEditorApiClient]);
+
+  if (!state.loading && !state.design) {
+    return (
+      <div data-testid={errorMessageTestId}>
+        Unexpected Error: {state.error}
+      </div>
+    );
+  }
+
+  return (
+    <>
+      {state.loading ? (
+        <div data-testid={loadingMessageTestId}>Loading...</div>
+      ) : null}
+      <Editor design={state.design}></Editor>;
+    </>
+  );
 };
