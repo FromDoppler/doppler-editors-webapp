02e20a127920c084a1044c577f9b9acb35a2522e
Andrés Moschini <andresmoschini@gmail.com>
2022-01-25 14:00:02 -0300

    chore: add better links to test behavior

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -12,22 +12,13 @@ export function Main() {
         </div>
         <nav className="flex-row-center">
           <div>
-            {/* Links to this own app (using history API) */}
             <Link to="/invoices">Invoices</Link> |{" "}
             <Link to="/expenses">Expenses</Link> |{" "}
-            <Link to="/wrong">Wrong</Link>
-          </div>
-          <div>
-            {/* links to this own app (using real browser navigation, assuming that `editors` is the
-            base path) */}
-            <a href="/editors/invoices">Invoices</a> |{" "}
-            <a href="/editors/expenses">Expenses</a> |{" "}
-            <a href="/editors/wrong">Wrong</a>
-          </div>
-          <div>
-            {/* links to Doppler WebApp app (assuming that domain is shared) */}
-            <a href="/login">Login</a> | <a href="/signup">Sign Up</a> |{" "}
-            <a href="/dashboard">Dashboard</a> | <a href="/wrong">Wrong</a>
+            <Link to="/campaigns/123">campaigns/123</Link> |{" "}
+            <Link to="/campaigns/456">campaigns/456</Link> |{" "}
+            <Link to="/campaigns/789">campaigns/789</Link> |{" "}
+            <Link to="/templates/1">/templates/1</Link> |{" "}
+            <a href="/login">Login</a>
           </div>
         </nav>
       </header>
