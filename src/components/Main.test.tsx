08e7f75ef347fa2e53e73c2b08f74a9fa221b81a
Andrés Moschini <andresmoschini@gmail.com>
2022-01-25 16:19:32 -0300

    test: improve Main.test.tsx structure

--- src/components/Main.test.tsx
+++ src/components/Main.test.tsx
@@ -2,12 +2,14 @@ import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { Main } from "./Main";
 
-test("renders learn react link", () => {
-  render(
-    <BrowserRouter>
-      <Main />
-    </BrowserRouter>
-  );
-  const linkElement = screen.getByText(/Editors WebApp/i);
-  expect(linkElement).toBeInTheDocument();
+describe(Main.name, () => {
+  it("renders learn react link", () => {
+    render(
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
+    );
+    const linkElement = screen.getByText(/Editors WebApp/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
