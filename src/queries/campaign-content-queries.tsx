0c916c61f69c90d9424be803dda4333aaf8dff19
Andrés Moschini <andresmoschini@gmail.com>
2022-02-01 14:56:54 -0300

    refactor: make useGetCampaignContent implementation explicit and verbose

--- src/queries/campaign-content-queries.tsx
+++ src/queries/campaign-content-queries.tsx
@@ -1,21 +1,33 @@
-import { useQuery } from "react-query";
+import { Design } from "react-email-editor";
+import { QueryFunction, useQuery } from "react-query";
 import { useAppServices } from "../components/AppServicesContext";
+
+type getCampaignContentQueryKey = {
+  scope: string;
+  idCampaign: string;
+}[];
 
 export const useGetCampaignContent = (idCampaign: string) => {
   const { htmlEditorApiClient } = useAppServices();
 
-  const query = useQuery({
-    queryKey: [
-      {
-        scope: "campaign-contents",
-        idCampaign,
-      },
-    ],
-    queryFn: async (context) => {
-      const [{ idCampaign }] = context.queryKey;
-      const result = await htmlEditorApiClient.getCampaignContent(idCampaign);
-      return result.value;
+  const queryKey: getCampaignContentQueryKey = [
+    {
+      scope: "campaign-contents",
+      idCampaign,
     },
+  ];
+
+  const queryFn: QueryFunction<Design, getCampaignContentQueryKey> = async (
+    context
+  ) => {
+    const [{ idCampaign }] = context.queryKey;
+    const result = await htmlEditorApiClient.getCampaignContent(idCampaign);
+    return result.value;
+  };
+
+  const query = useQuery({
+    queryKey,
+    queryFn,
     refetchOnMount: false,
     refetchOnWindowFocus: false,
     refetchOnReconnect: false,
