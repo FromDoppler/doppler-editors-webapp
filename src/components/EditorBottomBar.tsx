f9ab8a853deacedcc47bcc1474a30d561d8c5a46
mcardinali <mcardinali@makingsense.com>
2022-04-20 17:32:18 -0300

    fix: add click behaviour to bottom bar next button

--- src/components/EditorBottomBar.tsx
+++ src/components/EditorBottomBar.tsx
@@ -1,17 +1,22 @@
 import "./EditorBottomBar.css";
 import { FormattedMessage } from "react-intl";
 
-interface EditorBottomBarProps {}
+interface EditorBottomBarProps {
+  nextUrl: string;
+}
 
-export const EditorBottomBar = ({ ...otherProps }: EditorBottomBarProps) => {
+export const EditorBottomBar = ({
+  nextUrl,
+  ...otherProps
+}: EditorBottomBarProps) => {
   return (
     <div className="ed-cta-footer" {...otherProps}>
       <button type="button" className="dp-button button-medium secondary-green">
         <FormattedMessage id="exit_edit_later" />
       </button>
-      <button type="button" className="dp-button button-medium primary-green">
+      <a className="dp-button button-medium primary-green" href={nextUrl}>
         <FormattedMessage id="continue" />
-      </button>
+      </a>
     </div>
   );
 };
