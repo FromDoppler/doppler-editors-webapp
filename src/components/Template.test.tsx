19d3252f1fca5210561ad61efa3413500670584b
Andres Moschini <andresmoschini@gmail.com>
2022-11-10 16:06:21 -0300

    feat: allow to open templates

---
+++ src/components/Template.test.tsx
@@ -0,0 +1,151 @@
+import { renderEditor } from "../utils/testPortalUtils";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import { AppServices } from "../abstractions";
+import { HtmlEditorApiClient } from "../abstractions/html-editor-api-client";
+import { AppServicesProvider } from "./AppServicesContext";
+import { TestDopplerIntlProvider } from "./i18n/TestDopplerIntlProvider";
+import { screen } from "@testing-library/react";
+import { editorTopBarTestId, errorMessageTestId, Template } from "./Template";
+
+jest.mock("./LoadingScreen", () => ({
+  LoadingScreen: () => <div>Loading...</div>,
+}));
+
+const dopplerLegacyBaseUrl = "http://dopplerlegacybaseurl";
+const baseAppServices = {
+  appConfiguration: {
+    unlayerProjectId: 12345,
+    unlayerEditorManifestUrl: "unlayerEditorManifestUrl",
+    loaderUrl: "loaderUrl",
+    dopplerLegacyBaseUrl,
+    dopplerExternalUrls: {
+      home: "https://dopplerexternalurls.home/",
+      campaigns: "https://dopplerexternalurls.campaigns/",
+      lists: "https://dopplerexternalurls.lists/",
+      controlPanel: "https://dopplerexternalurls.controlpanel/",
+    },
+  },
+} as AppServices;
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      cacheTime: 0,
+    },
+  },
+});
+
+describe(Template.name, () => {
+  it("should show loading and then error when getTemplate is not successful", async () => {
+    // Arrange
+    const idTemplate = "1234";
+
+    let rejectGetTemplatePromise: any;
+    const getTemplate = jest.fn(
+      () =>
+        new Promise((_, reject) => {
+          rejectGetTemplatePromise = reject;
+        })
+    );
+
+    const htmlEditorApiClient = {
+      getTemplate,
+    } as unknown as HtmlEditorApiClient;
+
+    // Act
+    renderEditor(
+      <QueryClientProvider client={queryClient}>
+        <AppServicesProvider
+          appServices={{ ...baseAppServices, htmlEditorApiClient }}
+        >
+          <TestDopplerIntlProvider>
+            <MemoryRouter initialEntries={[`/${idTemplate}`]}>
+              <Routes>
+                <Route path="/:idTemplate" element={<Template />} />
+              </Routes>
+            </MemoryRouter>
+          </TestDopplerIntlProvider>
+        </AppServicesProvider>
+      </QueryClientProvider>
+    );
+
+    // Assert
+    expect(getTemplate).toHaveBeenCalledWith(idTemplate);
+
+    screen.getByText("Loading...");
+
+    const topBarMustBeNull = screen.queryByTestId(editorTopBarTestId);
+    expect(topBarMustBeNull).toBeNull();
+
+    const errorMessageEl = screen.queryByTestId(errorMessageTestId);
+    expect(errorMessageEl).toBeNull();
+
+    // Act
+    rejectGetTemplatePromise(true);
+
+    // Assert
+    await screen.findByTestId(errorMessageTestId);
+
+    expect(() => screen.getByText("Loading...")).toThrow();
+
+    const editorTobBarEl = screen.queryByTestId(editorTopBarTestId);
+    expect(editorTobBarEl).toBeNull();
+  });
+
+  it("should show EmailEditor when the getTemplate is successful", async () => {
+    // Arrange
+    const idTemplate = "1234";
+
+    let resolveGetTemplatePromise: any;
+    const getTemplate = jest.fn(
+      () =>
+        new Promise((resolve) => {
+          resolveGetTemplatePromise = resolve;
+        })
+    );
+
+    const htmlEditorApiClient = {
+      getTemplate,
+    } as unknown as HtmlEditorApiClient;
+
+    // Act
+    renderEditor(
+      <QueryClientProvider client={queryClient}>
+        <AppServicesProvider
+          appServices={{ ...baseAppServices, htmlEditorApiClient }}
+        >
+          <TestDopplerIntlProvider>
+            <MemoryRouter initialEntries={[`/${idTemplate}`]}>
+              <Routes>
+                <Route path="/:idTemplate" element={<Template />} />
+              </Routes>
+            </MemoryRouter>
+          </TestDopplerIntlProvider>
+        </AppServicesProvider>
+      </QueryClientProvider>
+    );
+
+    // Assert
+    screen.getByText("Loading...");
+
+    const errorMessageEl = screen.queryByTestId(errorMessageTestId);
+    expect(errorMessageEl).toBeNull();
+
+    expect(getTemplate).toHaveBeenCalledWith(idTemplate);
+
+    // Act
+    resolveGetTemplatePromise({ success: true, value: {} });
+
+    // Assert
+    await screen.findByTestId(editorTopBarTestId);
+
+    expect(() => screen.getByText("Loading...")).toThrow();
+
+    const errorMessageEl2 = screen.queryByTestId(errorMessageTestId);
+    expect(errorMessageEl2).toBeNull();
+
+    screen.getByTestId(editorTopBarTestId);
+  });
+});
--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -1,3 +1,61 @@
+import { useParams } from "react-router-dom";
+import { useSingletonEditor } from "./SingletonEditor";
+import { EditorTopBar } from "./EditorTopBar";
+import { useGetTemplate } from "../queries/template-queries";
+import { Header } from "./Header";
+import { LoadingScreen } from "./LoadingScreen";
+import { Content } from "../abstractions/domain/content";
+
+export const errorMessageTestId = "error-message";
+export const editorTopBarTestId = "editor-top-bar-message";
+
 export const Template = () => {
-  return <div>Not available! Pending for implementation</div>;
+  const { idTemplate } = useParams() as Readonly<{
+    idTemplate: string;
+  }>;
+
+  const templateQuery = useGetTemplate(idTemplate);
+
+  const { save } = useSingletonEditor(
+    {
+      initialContent: templateQuery.data,
+      onSave: (content: Content) => {
+        console.log(content);
+        // TODO: Implement it
+        // merge with other information like template name
+        // save
+      },
+    },
+    [templateQuery.data, idTemplate]
+  );
+
+  if (templateQuery.error) {
+    return (
+      <div data-testid={errorMessageTestId}>
+        Unexpected Error: <pre>{JSON.stringify(templateQuery.error)}</pre>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      {templateQuery.isLoading ? (
+        <LoadingScreen />
+      ) : (
+        <>
+          <Header>
+            <EditorTopBar
+              data-testid={editorTopBarTestId}
+              onSave={save}
+              title={
+                templateQuery.data?.templateName
+                  ? templateQuery.data.templateName
+                  : ""
+              }
+            />
+          </Header>
+        </>
+      )}
+    </>
+  );
 };
