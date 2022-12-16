835cf5dedf4fc40544832ad556f8d6e52f9c736f
Andres Moschini <andresmoschini@gmail.com>
2022-12-16 15:19:14 -0300

    chore: component for CreateTemplateFromTemplate landing

---
+++ src/components/CreateTemplateFromTemplate.test.tsx
@@ -0,0 +1,201 @@
+import { CreateTemplateFromTemplate } from "./CreateTemplateFromTemplate";
+import { MemoryRouter, Route, Routes, useLocation } from "react-router-dom";
+import type { Location } from "@remix-run/router";
+import { render, waitFor, screen } from "@testing-library/react";
+import { AppServicesProvider } from "./AppServicesContext";
+import { AppServices } from "../abstractions";
+import { InitialEntry } from "history";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+function createTestContext() {
+  const templatesUrl = "https://dopplerexternalurls.templates/";
+  const newTemplateId = "987";
+  const locationRef = {
+    value: undefined as Location | undefined,
+  };
+  const LocationInterceptorElement = () => {
+    locationRef.value = useLocation();
+    return null;
+  };
+
+  const htmlEditorApiClientDouble = {
+    createTemplateFromTemplate: jest.fn(),
+  };
+  htmlEditorApiClientDouble.createTemplateFromTemplate.mockResolvedValue({
+    success: true,
+    value: { newTemplateId },
+  });
+
+  const setHref = jest.fn();
+
+  const windowDouble = {
+    console: {
+      error: jest.fn(),
+    },
+    location: {
+      href: "",
+    },
+  };
+
+  Object.defineProperty(windowDouble.location, "href", {
+    set: setHref,
+  });
+
+  const appServices = {
+    htmlEditorApiClient: htmlEditorApiClientDouble,
+    appConfiguration: {
+      dopplerExternalUrls: {
+        templates: templatesUrl,
+      },
+    },
+    window: windowDouble,
+  } as any as AppServices;
+
+  const destinationPageText = "DestinationPage";
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        cacheTime: 0,
+      },
+    },
+  });
+
+  const renderTest = (initialUrl: InitialEntry) =>
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter initialEntries={[initialUrl]}>
+          <AppServicesProvider appServices={appServices}>
+            <Routes>
+              <Route
+                path="templates/create-from-template/:idTemplate"
+                element={
+                  <>
+                    <CreateTemplateFromTemplate />
+                    <LocationInterceptorElement />
+                  </>
+                }
+              />
+              <Route
+                path="*"
+                element={
+                  <>
+                    <LocationInterceptorElement />
+                    <p>{destinationPageText}</p>
+                  </>
+                }
+              />
+            </Routes>
+          </AppServicesProvider>
+        </MemoryRouter>
+      </QueryClientProvider>
+    );
+
+  return {
+    renderTest,
+    locationRef,
+    htmlEditorApiClientDouble,
+    destinationPageText,
+    newTemplateId,
+    templatesUrl,
+    windowDouble,
+    setHref,
+  };
+}
+
+describe(CreateTemplateFromTemplate.name, () => {
+  it("should redirect when API call is successful", async () => {
+    // Arrange
+    const idTemplate = "456";
+    const initialPath = `/templates/create-from-template/${idTemplate}`;
+    const initialSearch = "?abc=cde&x=true";
+    const initialUrl = `${initialPath}${initialSearch}`;
+
+    const {
+      renderTest,
+      locationRef,
+      htmlEditorApiClientDouble,
+      destinationPageText,
+      newTemplateId,
+    } = createTestContext();
+
+    const expectedPath = `/templates/${newTemplateId}`;
+
+    // Act
+    renderTest(initialUrl);
+
+    // Assert
+    expect(locationRef.value?.pathname).toBe(initialPath);
+    expect(locationRef.value?.search).toBe(initialSearch);
+    screen.getByTestId("loading-screen");
+
+    await waitFor(() => {
+      expect(
+        htmlEditorApiClientDouble.createTemplateFromTemplate
+      ).toHaveBeenCalledWith(idTemplate);
+    });
+
+    await waitFor(() => {
+      expect(locationRef.value?.pathname).toBe(expectedPath);
+      expect(locationRef.value?.search).toBe(initialSearch);
+    });
+
+    screen.getByText(destinationPageText);
+
+    expect(
+      htmlEditorApiClientDouble.createTemplateFromTemplate
+    ).toHaveBeenCalledTimes(1);
+  });
+
+  it("should show error and redirect when API call fails", async () => {
+    // Arrange
+    const idTemplate = "456";
+    const initialPath = `/templates/create-from-template/${idTemplate}`;
+    const initialSearch = "?abc=cde&x=true";
+    const initialUrl = `${initialPath}${initialSearch}`;
+
+    const {
+      renderTest,
+      locationRef,
+      htmlEditorApiClientDouble,
+      windowDouble,
+      setHref,
+      templatesUrl,
+    } = createTestContext();
+
+    const errorMessage = "ErrorMessage";
+    htmlEditorApiClientDouble.createTemplateFromTemplate.mockRejectedValue(
+      new Error(errorMessage)
+    );
+
+    // Act
+    renderTest(initialUrl);
+
+    // Assert
+    expect(locationRef.value?.pathname).toBe(initialPath);
+    expect(locationRef.value?.search).toBe(initialSearch);
+    screen.getByTestId("loading-screen");
+
+    await waitFor(() => {
+      expect(
+        htmlEditorApiClientDouble.createTemplateFromTemplate
+      ).toHaveBeenCalledWith(idTemplate);
+    });
+
+    await waitFor(() => {
+      expect(windowDouble.console.error).toHaveBeenCalledWith(
+        "Error creating template from template",
+        expect.objectContaining({ message: errorMessage })
+      );
+    });
+
+    expect(setHref).toHaveBeenCalledTimes(1);
+    expect(setHref).toHaveBeenCalledWith(templatesUrl);
+
+    expect(windowDouble.console.error).toHaveBeenCalledTimes(1);
+    expect(
+      htmlEditorApiClientDouble.createTemplateFromTemplate
+    ).toHaveBeenCalledTimes(1);
+  });
+});
