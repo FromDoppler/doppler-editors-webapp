8e69219063e40a10636bfa107937868200e0191b
Andres Moschini <andresmoschini@gmail.com>
2023-05-23 13:12:46 -0300

    refactor: make CustomMediaLibraryModal private

--- src/components/custom-media-library/CustomMediaLibraryModal.tsx
+++ src/components/custom-media-library/CustomMediaLibraryModal.tsx
@@ -4,7 +4,7 @@ import { useModal } from "react-modal-hook";
 import { noop } from "../../utils";
 import { CustomMediaLibrary } from "./CustomMediaLibrary";
 
-export const CustomMediaLibraryModal = ({
+const CustomMediaLibraryModal = ({
   cancel,
   selectImage,
 }: {
