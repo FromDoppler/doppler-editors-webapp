f9ddd29e2ee527a2f1c519576b5e960c0e815fe7
Andres Moschini <andresmoschini@gmail.com>
2023-06-23 16:48:52 -0300

    refactor: split generic confirmation modal hook from delete confirmation modal

--- src/components/confirmation/DeleteConfirmationModal.tsx
+++ src/components/confirmation/DeleteConfirmationModal.tsx
@@ -1,58 +1,35 @@
-import { CSSProperties, useCallback, useMemo, useState } from "react";
-import { useModal } from "react-modal-hook";
-import { noop, takeOneValue } from "../../utils";
-import { ConfirmationModal } from "./ConfirmationModal";
+import { CSSProperties } from "react";
+import { takeOneValue } from "../../utils";
+import { useConfirmationModal } from "./useConfirmationModal";
 
 // TODO: consider moving these styles to classes in the Style Guide
 const deleteButtonStyles: CSSProperties = { backgroundColor: "#E2574C" };
 
 export const useDeleteConfirmationModal = () => {
-  const [{ onConfirm, checkedImages }, setProps] = useState<{
-    onConfirm: () => void;
+  const { showConfirmationModal } = useConfirmationModal();
+
+  const showDeleteConfirmationModal = ({
+    checkedImages,
+    onConfirm,
+  }: {
     checkedImages: ReadonlySet<string>;
-  }>({
-    checkedImages: new Set(),
-    onConfirm: noop,
-  });
+    onConfirm: () => void;
+  }) =>
+    showConfirmationModal({
+      messageDescriptorId:
+        checkedImages.size === 1
+          ? "delete_images_confirmation_single"
+          : "delete_images_confirmation_multiple",
+      confirmationButtonDescriptorId: "delete",
+      confirmationButtonStyles: deleteButtonStyles,
+      values: {
+        firstName: takeOneValue(checkedImages),
+        itemsCount: checkedImages.size,
+      },
+      onConfirm,
+    });
 
-  const messageDescriptorId =
-    checkedImages.size === 1
-      ? "delete_images_confirmation_single"
-      : "delete_images_confirmation_multiple";
-
-  const values = useMemo(
-    () => ({
-      firstName: takeOneValue(checkedImages),
-      itemsCount: checkedImages.size,
-    }),
-    [checkedImages]
-  );
-
-  const [showModal, hideModal] = useModal(
-    () => (
-      <ConfirmationModal
-        confirmationButtonDescriptorId="delete"
-        messageDescriptorId={messageDescriptorId}
-        cancelationButtonDescriptorId="cancel"
-        confirmationButtonStyles={deleteButtonStyles}
-        values={values}
-        onCancel={hideModal}
-        onConfirm={() => {
-          onConfirm();
-          hideModal();
-        }}
-      />
-    ),
-    [onConfirm, checkedImages]
-  );
-
-  const showDeleteConfirmationModal = useCallback(
-    (props: { onConfirm: () => void; checkedImages: ReadonlySet<string> }) => {
-      setProps(props);
-      showModal();
-    },
-    [showModal]
-  );
-
-  return { showDeleteConfirmationModal };
+  return {
+    showDeleteConfirmationModal,
+  };
 };
