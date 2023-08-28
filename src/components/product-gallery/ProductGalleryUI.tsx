d36a8ec4a95372dd7b7207d6904743adaae01c6f
Andres Moschini <andresmoschini@gmail.com>
2023-08-28 12:13:13 -0300

    feat(preview): show product gallery modal (empty)

---
+++ src/components/product-gallery/ProductGalleryUI.tsx
@@ -0,0 +1,6 @@
+// TODO: implement it
+export const ProductGalleryUI = (props: any) => (
+  <code>
+    <pre>{JSON.stringify(props)}</pre>
+  </code>
+);
---
+++ src/components/product-gallery/product-gallery-behavior.ts
@@ -0,0 +1,5 @@
+// TODO: implement it
+export const useProductGalleryBehavior = (props: any) => {
+  console.log("useProductGalleryBehavior", props);
+  return props;
+};
