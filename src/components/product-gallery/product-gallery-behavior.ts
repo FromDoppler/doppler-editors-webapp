b5acd65f2eacbd877519bb27fbff394649165bf4
Andres Moschini <andresmoschini@gmail.com>
2023-09-04 10:04:12 -0300

    feat(preview): allow to select items in the products gallery

--- src/components/product-gallery/product-gallery-behavior.ts
+++ src/components/product-gallery/product-gallery-behavior.ts
@@ -1,48 +1,97 @@
-import { noop } from "../../utils";
-import { GalleryItem } from "../base-gallery/GalleryItem";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import {
+  defaultQueryParameters,
+  useGetProductsGallery,
+} from "../../queries/products-gallery-queries";
+import { ProductGalleryValue } from "../../abstractions/domain/product-gallery";
+import { noop, useDebounce } from "../../utils";
 import { SortingProductsPair } from "./HeaderSortProductsDropdown";
 
-// TODO: implement it
-export const useProductGalleryBehavior = <TValue>({
+export const useProductGalleryBehavior = ({
   cancel,
   selectItem,
-  ...rest
-}: { cancel: () => void; selectItem: (item: TValue) => void } & Record<
-  string,
-  any
->) => {
-  console.log("useProductGalleryBehavior", { selectItem, ...rest });
-  // TODO: implement it
-  const checkedItemIds = new Set<string>([]);
-  const debouncedSearchTerm = "";
-  const fetchNextPage = noop;
-  const hasNextPage = false;
-  const isFetching = false;
-  const items: GalleryItem<TValue>[] = [];
-  const searchTerm = "";
+}: {
+  cancel: () => void;
+  selectItem: (item: ProductGalleryValue) => void;
+}) => {
+  const [searchTerm, setSearchTerm] = useState(
+    defaultQueryParameters.searchTerm,
+  );
+  const [sortingCriteria /*, setSortingCriteria*/] = useState(
+    defaultQueryParameters.sortingCriteria,
+  );
+  const [sortingDirection /*, setSortingDirection*/] = useState(
+    defaultQueryParameters.sortingDirection,
+  );
+
+  const parametersToDebounce = useMemo(
+    () => ({ searchTerm, sortingCriteria, sortingDirection }),
+    [searchTerm, sortingCriteria, sortingDirection],
+  );
+
+  const debouncedQueryParameters = useDebounce(parametersToDebounce, 300);
+
+  const { isFetching, items, hasNextPage, fetchNextPage } =
+    useGetProductsGallery(debouncedQueryParameters);
+
+  const [checkedItemIds, setCheckedItemIds] = useState<ReadonlySet<string>>(
+    new Set(),
+  );
+
+  // Sanitize checkedItemIds based on existing items
+  useEffect(() => {
+    if (isFetching) {
+      return;
+    }
+    const newCheckedItemIds = items.filter((x) =>
+      checkedItemIds.has(x.productUrl),
+    );
+    // It is only different when we should sanitize
+    if (newCheckedItemIds.length !== checkedItemIds.size) {
+      setCheckedItemIds(new Set(newCheckedItemIds.map((x) => x.productUrl)));
+    }
+  }, [isFetching, items, checkedItemIds]);
+
+  const toggleCheckedItemSimpleSelection = useCallback(
+    (id: string) =>
+      setCheckedItemIds(checkedItemIds.has(id) ? new Set() : new Set([id])),
+    [checkedItemIds],
+  );
+
+  // TODO: implement these
   const selectCheckedItem = null;
-  const setSearchTerm = noop;
   const setSorting = noop;
   const sorting: SortingProductsPair = {
     criteria: "PRICE",
     direction: "ASCENDING",
   };
-  const toggleCheckedItem = noop;
+
+  const galleryItems = useMemo(
+    () =>
+      // TODO: update it based on the real data
+      items.map((x) => ({
+        text: x.title,
+        thumbnailUrl: x.imageUrl,
+        id: x.productUrl,
+        item: x,
+      })),
+    [items],
+  );
 
   return {
     cancel,
     checkedItemIds,
-    debouncedSearchTerm,
+    debouncedSearchTerm: debouncedQueryParameters.searchTerm,
     fetchNextPage,
     hasNextPage,
     isFetching,
-    items,
+    items: galleryItems,
     searchTerm,
     selectCheckedItem,
     selectItem,
     setSearchTerm,
     setSorting,
     sorting,
-    toggleCheckedItem,
+    toggleCheckedItem: toggleCheckedItemSimpleSelection,
   };
 };
